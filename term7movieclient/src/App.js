59b7c4c772917b0b516893e1334fd0e3beb9f83c
KN <nguyentuanpkn@gmail.com>
2022-05-14 13:16:02 -0700

    Change client

---
+++ term7movieclient/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import Home from './Home';
+import './App.css';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home></Home>} />
+  </Routes>
+);
+
+export default App;
