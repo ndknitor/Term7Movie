01db7e82f7c563f4bc02e44d461fd3a87eab5774
KN <nguyentuanpkn@gmail.com>
2022-05-21 12:20:37 -0700

    Test push

--- term7movieclient/src/App.js
+++ term7movieclient/src/App.js
@@ -1,22 +1,20 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Home from './Home';
 import './App.css';
 
 function App() {
   useEffect(() => {
-    axios.get('/api/home/hello').then(res => {
+    axios.get('/api/public/hello').then(res => {
       console.log(res.data);
       console.log('asdasdas');
     });
   }, []);
   return (
-    <BrowserRouter basename='/Term7Movie'>
-      <Routes>
-        <Route path="/" element={<Home></Home>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home></Home>} />
+    </Routes>
   );
 };
 
