22556d88190a885acb1a5f419783821adb099bb3
x <x>
2022-06-02 08:45:56 +0700

    Auto stash before rebase of "origin/master"
    Add paging list

---
+++ Term7MovieCore/Data/Collections/PagingList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Term7MovieCore.Data.Collections
+{
+    public class PagingList<T> : List<T>
+    {
+        public const int DefaultPageSize = 10;
+        public const int DefaultPage = 1;
+        public int PageSize { set; get; } = DefaultPageSize;
+        public int Page { set; get; } = DefaultPage;
+
+        public PagingList() : base() { }
+        public PagingList(int pageSize, int page)
+        {
+            PageSize = pageSize;
+            Page = page;
+        }
+    }
+}
