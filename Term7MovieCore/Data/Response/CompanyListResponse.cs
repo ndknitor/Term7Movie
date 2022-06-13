059529bffecea1515a6c25f24939d5923733232a
Tien Xuan <deathsida001>
2022-06-13 23:49:31 +0700

    Transaction history, company list

---
+++ Term7MovieCore/Data/Response/CompanyListResponse.cs
@@ -0,0 +1,9 @@
+﻿using Term7MovieCore.Data.Dto;
+
+namespace Term7MovieCore.Data.Response
+{
+    public class CompanyListResponse : ParentResponse
+    {
+        public IEnumerable<CompanyDto> Companies { set; get; }
+    }
+}
---
+++ Term7MovieCore/Data/Response/TransactionHistoryListResponse.cs
@@ -0,0 +1,9 @@
+﻿using Term7MovieCore.Entities;
+
+namespace Term7MovieCore.Data.Response
+{
+    public class TransactionHistoryListResponse : ParentResponse
+    {
+        public IEnumerable<TransactionHistory> History { set; get; }
+    }
+}
