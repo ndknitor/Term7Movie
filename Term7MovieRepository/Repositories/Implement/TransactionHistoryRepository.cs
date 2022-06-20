2d7b4c1db6c9da8b3f764d9ab0d33767e16c22c0
saphareong <tranhainam12072001@gmail.com>
2022-06-20 18:49:58 +0700

    Is It over yet

--- Term7MovieRepository/Repositories/Implement/TransactionHistoryRepository.cs
+++ Term7MovieRepository/Repositories/Implement/TransactionHistoryRepository.cs
@@ -141,153 +141,155 @@ namespace Term7MovieRepository.Repositories.Implement
         public async Task<TicketSoldDTO> GetQuickTicketSoldInTwoRecentWeek(int companyid, /*bool Comparable,*/
             DateTime ThisMondayWeek, DateTime MondayPreviousWeek, DateTime SundayPreviousWeek)
         {
-            if (!await _context.Database.CanConnectAsync())
-                throw new Exception("DBCONNECTION");
+            throw new NotImplementedException();
+            //if (!await _context.Database.CanConnectAsync())
+            //    throw new Exception("DBCONNECTION");
 
-            //Comprable mean it can be compare two week together to create 2 different number
-            //if (Comparable)
+            ////Comprable mean it can be compare two week together to create 2 different number
+            ////if (Comparable)
+            ////{
+
+            ////}
+
+            //var TicketIds = await _context.Theaters
+            //                    .Include(a => a.Showtimes)
+            //                    .Where(a => a.CompanyId == 1)
+            //                    .SelectMany(a => a.Showtimes)
+            //                    .SelectMany(a => a.Tickets)
+            //                    .Select(a => a.Id)
+            //                    .ToListAsync();
+            ////if (TicketIds.Count == 0) return null;
+            //var showtimeids = await _context.Theaters
+            //                    .Include(a => a.Showtimes)
+            //                    .Where(a => a.CompanyId == 1)
+            //                    .SelectMany(a => a.Showtimes)
+            //                    .Select(a => a.Id).ToListAsync();
+            ////Getting ticket id in 2 different timeline
+            //var TicketIdsOld = await _context.Showtimes
+            //                        .Include(a => a.Tickets)
+            //                        .Where(a => a.Tickets.Any
+            //                        (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
+            //                            && a.StartTime <= DateTime.UtcNow.Date && a.StartTime >= ThisMondayWeek)
+            //                        .SelectMany(a => a.Tickets).Select(a => a.Id).ToListAsync();
+            //var TicketIdsNew = await _context.Showtimes
+            //                        .Include(a => a.Tickets)
+            //                        .Where(a => a.Tickets.Any
+            //                        (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
+            //                            && a.StartTime <= SundayPreviousWeek && a.StartTime >= MondayPreviousWeek)
+            //                        .SelectMany(a => a.Tickets).Select(a => a.Id).ToListAsync();
+            ////start to track down the quanity sold successfully
+            //var OldTicketSold = await _context.TransactionHistories
+            //                                    .Where(a => TicketIdsOld.Contains(a.TicketId))
+            //                                    .CountAsync();
+            //var NewTicketSold = await _context.TransactionHistories
+            //                                    .Where(a => TicketIdsNew.Contains(a.TicketId))
+            //                                    .CountAsync();
+            //var TotalTicketSold = await _context.TransactionHistories
+            //                                    .CountAsync(a => TicketIds.Contains(a.TicketId));//oh so where wasn't necessary D:
+            //if (OldTicketSold == 0 && NewTicketSold == 0 && TotalTicketSold == 0)
             //{
-
+            //    NewTicketSold = 1;
+            //    OldTicketSold = 1;
+            //    TotalTicketSold = 1;
             //}
-
-            var TicketIds = await _context.Theaters
-                                .Include(a => a.Showtimes)
-                                .Where(a => a.CompanyId == 1)
-                                .SelectMany(a => a.Showtimes)
-                                .SelectMany(a => a.Tickets)
-                                .Select(a => a.Id)
-                                .ToListAsync();
-            //if (TicketIds.Count == 0) return null;
-            var showtimeids = await _context.Theaters
-                                .Include(a => a.Showtimes)
-                                .Where(a => a.CompanyId == 1)
-                                .SelectMany(a => a.Showtimes)
-                                .Select(a => a.Id).ToListAsync();
-            //Getting ticket id in 2 different timeline
-            var TicketIdsOld = await _context.Showtimes
-                                    .Include(a => a.Tickets)
-                                    .Where(a => a.Tickets.Any
-                                    (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
-                                        && a.StartTime <= DateTime.UtcNow.Date && a.StartTime >= ThisMondayWeek)
-                                    .SelectMany(a => a.Tickets).Select(a => a.Id).ToListAsync();
-            var TicketIdsNew = await _context.Showtimes
-                                    .Include(a => a.Tickets)
-                                    .Where(a => a.Tickets.Any
-                                    (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
-                                        && a.StartTime <= SundayPreviousWeek && a.StartTime >= MondayPreviousWeek)
-                                    .SelectMany(a => a.Tickets).Select(a => a.Id).ToListAsync();
-            //start to track down the quanity sold successfully
-            var OldTicketSold = await _context.TransactionHistories
-                                                .Where(a => TicketIdsOld.Contains(a.TicketId))
-                                                .CountAsync();
-            var NewTicketSold = await _context.TransactionHistories
-                                                .Where(a => TicketIdsNew.Contains(a.TicketId))
-                                                .CountAsync();
-            var TotalTicketSold = await _context.TransactionHistories
-                                                .CountAsync(a => TicketIds.Contains(a.TicketId));//oh so where wasn't necessary D:
-            if (OldTicketSold == 0 && NewTicketSold == 0 && TotalTicketSold == 0)
-            {
-                NewTicketSold = 1;
-                OldTicketSold = 1;
-                TotalTicketSold = 1;
-            }
-            TicketSoldDTO dto = new TicketSoldDTO();
-            dto.TotalTicketSoldQuantity = TotalTicketSold;
-            dto.OldTicketSoldQuantity = OldTicketSold;
-            dto.NewTicketSoldQuantity = NewTicketSold;
-            if(NewTicketSold > OldTicketSold)
-            {
-                dto.IsTicketSoldUpOrDown = true;
-                dto.PercentTicketSoldChange = (float)100F - ((float)OldTicketSold * (float)100F / (float)NewTicketSold);
-            }
-            else if(NewTicketSold < OldTicketSold)
-            {
-                dto.IsTicketSoldUpOrDown = false;
-                dto.PercentTicketSoldChange = (float)100F - ((float)NewTicketSold * (float)100F / (float)OldTicketSold);
-            }
-            else if(NewTicketSold == OldTicketSold)
-            {
-                dto.IsTicketSoldUpOrDown = true;
-                dto.PercentTicketSoldChange = 0.69F;
-            }
-            return dto;
+            //TicketSoldDTO dto = new TicketSoldDTO();
+            //dto.TotalTicketSoldQuantity = TotalTicketSold;
+            //dto.OldTicketSoldQuantity = OldTicketSold;
+            //dto.NewTicketSoldQuantity = NewTicketSold;
+            //if(NewTicketSold > OldTicketSold)
+            //{
+            //    dto.IsTicketSoldUpOrDown = true;
+            //    dto.PercentTicketSoldChange = (float)100F - ((float)OldTicketSold * (float)100F / (float)NewTicketSold);
+            //}
+            //else if(NewTicketSold < OldTicketSold)
+            //{
+            //    dto.IsTicketSoldUpOrDown = false;
+            //    dto.PercentTicketSoldChange = (float)100F - ((float)NewTicketSold * (float)100F / (float)OldTicketSold);
+            //}
+            //else if(NewTicketSold == OldTicketSold)
+            //{
+            //    dto.IsTicketSoldUpOrDown = true;
+            //    dto.PercentTicketSoldChange = 0.69F;
+            //}
+            //return dto;
         }
 
         public async Task<IncomeDTO> GetQuickTicketStonkOrStinkInTwoRecentWeek(int companyid, /*bool Comparable,*/
             DateTime ThisMondayWeek, DateTime MondayPreviousWeek, DateTime SundayPreviousWeek)
         {
-            if (!await _context.Database.CanConnectAsync())
-                throw new Exception("DBCONNECTION");
+            throw new NotImplementedException();
+            //if (!await _context.Database.CanConnectAsync())
+            //    throw new Exception("DBCONNECTION");
 
-            //Comprable mean it can be compare two week together to create 2 different number
-            //if (Comparable)
+            ////Comprable mean it can be compare two week together to create 2 different number
+            ////if (Comparable)
+            ////{
+
+            ////}
+
+            //var TicketIds = await _context.Theaters
+            //                    .Include(a => a.Showtimes)
+            //                    .Where(a => a.CompanyId == 1)
+            //                    .SelectMany(a => a.Showtimes)
+            //                    .SelectMany(a => a.Tickets)
+            //                    .Select(a => a.Id)
+            //                    .ToListAsync();
+            ////if (TicketIds.Count == 0) return null;
+            //var showtimeids = await _context.Theaters
+            //                    .Include(a => a.Showtimes)
+            //                    .Where(a => a.CompanyId == 1)
+            //                    .SelectMany(a => a.Showtimes)
+            //                    .Select(a => a.Id).ToListAsync();
+            ////Getting ticket id in 2 different timeline
+            //var OldIncome = await _context.Showtimes
+            //                        .Include(a => a.Tickets)
+            //                        .Where(a => a.Tickets.Any
+            //                        (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
+            //                            && a.StartTime <= SundayPreviousWeek && a.StartTime >= MondayPreviousWeek)
+            //                        .SelectMany(a => a.Tickets)
+            //                        .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
+            //                        .SumAsync(a => a.ReceivePrice);
+            //var NewIcome = await _context.Showtimes
+            //                        .Include(a => a.Tickets)
+            //                        .Where(a => a.Tickets.Any
+            //                        (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
+            //                            && a.StartTime <= DateTime.UtcNow.Date && a.StartTime >= ThisMondayWeek)
+            //                        .SelectMany(a => a.Tickets)
+            //                        .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
+            //                        .SumAsync(a => a.ReceivePrice);
+            //var TotalIncome = await _context.Showtimes
+            //                        .Include(a => a.Tickets)
+            //                        .Where(a => a.Tickets.Any
+            //                        (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1)))
+            //                        .SelectMany(a => a.Tickets)
+            //                        .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
+            //                        .SumAsync(a => a.ReceivePrice);
+            //if(TotalIncome == 0 && NewIcome == 0 && OldIncome == 0)
             //{
-
+            //    TotalIncome = 1;
+            //    NewIcome = 1;
+            //    OldIncome = 1;
             //}
-
-            var TicketIds = await _context.Theaters
-                                .Include(a => a.Showtimes)
-                                .Where(a => a.CompanyId == 1)
-                                .SelectMany(a => a.Showtimes)
-                                .SelectMany(a => a.Tickets)
-                                .Select(a => a.Id)
-                                .ToListAsync();
-            //if (TicketIds.Count == 0) return null;
-            var showtimeids = await _context.Theaters
-                                .Include(a => a.Showtimes)
-                                .Where(a => a.CompanyId == 1)
-                                .SelectMany(a => a.Showtimes)
-                                .Select(a => a.Id).ToListAsync();
-            //Getting ticket id in 2 different timeline
-            var OldIncome = await _context.Showtimes
-                                    .Include(a => a.Tickets)
-                                    .Where(a => a.Tickets.Any
-                                    (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
-                                        && a.StartTime <= SundayPreviousWeek && a.StartTime >= MondayPreviousWeek)
-                                    .SelectMany(a => a.Tickets)
-                                    .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
-                                    .SumAsync(a => a.ReceivePrice);
-            var NewIcome = await _context.Showtimes
-                                    .Include(a => a.Tickets)
-                                    .Where(a => a.Tickets.Any
-                                    (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1))
-                                        && a.StartTime <= DateTime.UtcNow.Date && a.StartTime >= ThisMondayWeek)
-                                    .SelectMany(a => a.Tickets)
-                                    .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
-                                    .SumAsync(a => a.ReceivePrice);
-            var TotalIncome = await _context.Showtimes
-                                    .Include(a => a.Tickets)
-                                    .Where(a => a.Tickets.Any
-                                    (a => showtimeids.Contains(a.ShowTimeId != null ? a.ShowTimeId.Value : -1)))
-                                    .SelectMany(a => a.Tickets)
-                                    .Where(a => _context.TransactionHistories.FirstOrDefault(xxx => xxx.TicketId == a.Id) == null ? false : true)
-                                    .SumAsync(a => a.ReceivePrice);
-            if(TotalIncome == 0 && NewIcome == 0 && OldIncome == 0)
-            {
-                TotalIncome = 1;
-                NewIcome = 1;
-                OldIncome = 1;
-            }
-            IncomeDTO dto = new IncomeDTO();
-            dto.TotalIncome = TotalIncome;
-            dto.NewIncome = NewIcome;
-            dto.OldIncome = OldIncome;
-            if (NewIcome > OldIncome)
-            {
-                dto.IsIncomeUpOrDown = true;
-                dto.PercentIncomeChange = 100M - (OldIncome * 100M / NewIcome);
-            }
-            else if (NewIcome < OldIncome)
-            {
-                dto.IsIncomeUpOrDown = false;
-                dto.PercentIncomeChange = 100M - (NewIcome * 100M / OldIncome);
-            }
-            else if (NewIcome == OldIncome)
-            {
-                dto.IsIncomeUpOrDown = true;
-                dto.PercentIncomeChange = 0.69M;
-            }
-            return dto;
+            //IncomeDTO dto = new IncomeDTO();
+            //dto.TotalIncome = TotalIncome;
+            //dto.NewIncome = NewIcome;
+            //dto.OldIncome = OldIncome;
+            //if (NewIcome > OldIncome)
+            //{
+            //    dto.IsIncomeUpOrDown = true;
+            //    dto.PercentIncomeChange = 100M - (OldIncome * 100M / NewIcome);
+            //}
+            //else if (NewIcome < OldIncome)
+            //{
+            //    dto.IsIncomeUpOrDown = false;
+            //    dto.PercentIncomeChange = 100M - (NewIcome * 100M / OldIncome);
+            //}
+            //else if (NewIcome == OldIncome)
+            //{
+            //    dto.IsIncomeUpOrDown = true;
+            //    dto.PercentIncomeChange = 0.69M;
+            //}
+            //return dto;
         }
     }
 }
