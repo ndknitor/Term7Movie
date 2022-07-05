0d98118582f84552e35784e1156b8d30f463f3f9
saphareong <tranhainam12072001@gmail.com>
2022-07-05 18:46:39 +0700

    Momo dependency setup

--- term7moviemobile/lib/utils/constants.dart
+++ term7moviemobile/lib/utils/constants.dart
@@ -2,4 +2,9 @@ class Constants {
   static const String baseApiUrl = "https://api.fcinema.tk/api/v1/";
   static const String defaultAvatar = "assets/images/default-avatar.jpg";
   static const String defaultImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBCGH3ttKv97u7GnSxpSM4_ijDWF7yPb7GKmva3LU4_ZDJ-X2Jnfw2DVcwgsqmdome8Uo&usqp=CAU&fbclid=IwAR2ULi_nXOW9Lz9mbL-cmlIDwb7Rh8LnDtmtrb3u6pmM7JxN7IMBbktl5PE";
+
+  //Momo constants
+  static const String channelName = "com.yd.momo/momo_payment";
+  static const String methodRequestPayment = "requestPayment";
+  //end momo constants
 }
